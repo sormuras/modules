4d7b363796b1464591c115f02e5f7be19b0c97d0
Christian Stein <sormuras@gmail.com>
2019-03-31 08:57:29 +0200

    Move deploy commands into a dedicated script
    Issue #1

---
+++ .travis-deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+if [[ ! -f .travis-commit-message.md ]]; then
+	echo "Commit message file '.travis-commit-message.md' not found!"
+	exit 1
+fi
+
+git config --global user.name "Travis CI"
+git checkout master
+git add .
+git commit --file .travis-commit-message.md
+git remote rm origin
+git remote add origin https://${GH_TOKEN}@github.com/sormuras/modules.git
+git push origin master --dry-run
