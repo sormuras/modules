8c25ab092cb06d9e1f1f3f8e54fe02adfba848b3
Christian Stein <sormuras@gmail.com>
2021-03-15 17:36:32 +0100

    Update project-info

--- .bach/bach.info/module-info.java
+++ .bach/bach.info/module-info.java
@@ -1,8 +1,13 @@
 import com.github.sormuras.bach.ProjectInfo;
+import com.github.sormuras.bach.ProjectInfo.Tools;
 import com.github.sormuras.bach.project.JavaStyle;
 
-@ProjectInfo(version = "0-ea", format = JavaStyle.GOOGLE)
+@ProjectInfo(
+    version = "0-ea",
+    format = JavaStyle.GOOGLE,
+    tools = @Tools(limit = {"javac", "jar"}),
+    compileModulesForJavaRelease = 11,
+    includeSourceFilesIntoModules = true)
 module bach.info {
   requires com.github.sormuras.bach;
-  provides com.github.sormuras.bach.Bach.Provider with bach.info.Builder;
 }
